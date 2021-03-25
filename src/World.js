e04d0ea8d8a7d9fc526b21afca3011f0a9bab158
Dean Palm <dean@sxsw.com>
2021-03-24 20:58:32 -0500

    cellular map from rot js

--- src/World.js
+++ src/World.js
@@ -1,3 +1,5 @@
+import {Map} from 'rot-js'
+
 class World {
   constructor(width, height, tilesize) {
     this.width=width;
@@ -7,15 +9,21 @@ class World {
     for (let x = 0; x<this.width;x++){
       this.worldmap[x] = new Array(this.height)
     }
-    this.createRandomMap();
+    this.createCellularMap();
   }
 
-  createRandomMap(){
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        this.worldmap[x][y] = Math.round(Math.random());
+  createCellularMap(){
+    var map = new Map.Cellular(this.width, this.height, { connected: true });
+    map.randomize(0.5);
+    var userCallback = (x,y,value) => {
+      if (x === 0 || y === 0 || x === this.width - 1 || y === this.height -1) {
+        this.worldmap[x][y] = 1; // create walls around edges
+        return;
       }
-    }
+      this.worldmap[x][y] = value === 0 ? 1 : 0;
+    };
+    map.create(userCallback);
+    map.connect(userCallback, 1);
   }
 
   draw(context) {
