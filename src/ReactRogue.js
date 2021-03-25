d4ec6dc610b25a9a27e1d08db418df3458170661
Dean Palm <dean@sxsw.com>
2021-03-24 21:17:26 -0500

    the player is inside the world

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -5,16 +5,24 @@ import World from './World.js'
 
 const ReactRogue = ({width,height,tilesize}) => {
 const canvasRef = useRef()
-const [player,setPlayer] = useState(new Player(1, 2, tilesize));
+// const [player,setPlayer] = useState(new Player(1, 2, tilesize));
 const [world,setWorld] = useState(new World(width, height, tilesize));
 let inputManager = new InputManager();
 const handleInput = (action, data) => {
   console.log(`handle input: ${action}:${JSON.stringify(data)}`);
-  let newPlayer = new Player();
-  Object.assign(newPlayer, player);
-  newPlayer.move(data.x, data.y);
-  setPlayer(newPlayer);
+  let newWorld = new World();
+  Object.assign(newWorld, world);
+  newWorld.movePlayer(data.x, data.y);
+  setWorld(newWorld);
 };
+
+useEffect(()=> {
+  console.log('Create Map!');
+  let newWorld = new World();
+  Object.assign(newWorld, world);
+  newWorld.createCellularMap();
+  setWorld(newWorld);
+}, []);
 
 useEffect(()=>{
   console.log('Bind input');
@@ -31,7 +39,6 @@ useEffect(()=> {
  const ctx = canvasRef.current.getContext('2d');
  ctx.clearRect(0, 0, width * tilesize, height * tilesize)
  world.draw(ctx);
- player.draw(ctx);
 });
 return (
   <canvas
--- src/World.js
+++ src/World.js
@@ -1,16 +1,26 @@
-import {Map} from 'rot-js'
+import {Map} from 'rot-js';
+import Player from './Player.js';
 
 class World {
   constructor(width, height, tilesize) {
     this.width=width;
     this.height=height;
     this.tilesize=tilesize;
+    this.entities = [new Player(0,0,16)];
     this.worldmap = new Array(this.width);
     for (let x = 0; x<this.width;x++){
       this.worldmap[x] = new Array(this.height)
     }
-    this.createCellularMap();
   }
+
+  get player(){
+    return this.entities[0];
+  }
+
+  movePlayer(dx,dy){
+    this.player.move(dx,dy);
+  }
+
 
   createCellularMap(){
     var map = new Map.Cellular(this.width, this.height, { connected: true });
@@ -32,6 +42,9 @@ class World {
         if (this.worldmap[x][y] === 1) this.drawWall(context, x, y);
       }
     }
+    this.entities.forEach(entity => {
+      entity.draw(context);
+    })
   }
 
   drawWall(context,x,y) {
